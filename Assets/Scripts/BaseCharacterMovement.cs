65a6641856b22bef87cc4157611769c51464a887
Lucas Pietruschka <lucaspietruschka@gmx.de>
2025-05-13 10:05:29 +0200

    Basic Player Movement as Placeholder

--- Assets/Scripts/BaseCharacterMovement.cs
+++ Assets/Scripts/BaseCharacterMovement.cs
@@ -1,16 +1,30 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
+using static UnityEngine.InputSystem.InputAction;
 
 public class BaseCharacterMovement : MonoBehaviour
 {
+    private Vector2 movementInput;
+    private Transform camTransform;
+    [SerializeField] private float movementSpeed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
-        
+        camTransform = Camera.main.transform;
+   
     }
-
-    // Update is called once per frame
-    void Update()
+    public void Movement(CallbackContext ctx)
     {
-        
+        movementInput = ctx.ReadValue<Vector2>();
+    }
+    private void FixedUpdate()
+    {
+
+
+        var movementDirection = movementInput.x * camTransform.right + movementInput.y * Vector3.ProjectOnPlane(camTransform.forward, Vector3.up).normalized;
+
+        transform.Translate(movementDirection * Time.deltaTime * movementSpeed );
+
     }
 }
